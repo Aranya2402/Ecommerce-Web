2aca80fc164eee0a4c7ee44356e3b10f20daf4e5
Aranya2402 <aranya24arii@gmail.com>
2024-09-30 15:05:34 +0530

    get single

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -1,9 +1,11 @@
 const Product = require('../models/productModel');
-
-exports.getProducts = (req, res, next) => {
+//Get products - /api/v1/products
+exports.getProducts = async (req, res, next) => {
+    const products = await Product.find();
     res.status(200).json({
         success: true,
-        message: "This route will show all the products in the database"
+        count: products.length,
+        products
     });
 }
 //create Product -/api/v1/product/new
@@ -12,5 +14,23 @@ exports.newProduct = async (req, res, next)=>{
   res.status(201).json({
     success: true,
     product //product:product
+  })
+}
+
+
+// Get Single product 
+exports.getSingleProduct = async(req, res, next) => {
+ const product = await Product.findById(req.params.id);
+
+ if(!product) {
+  return res.status(404).json({
+    success: false,
+    message: "Product no found"
+  });
+ }
+
+  res.status(201).json({
+    success:true,
+    product
   })
 }
