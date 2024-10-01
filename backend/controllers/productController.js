9c24652df92ee6708b3f948030d64190ce62b495
Aranya2402 <aranya24arii@gmail.com>
2024-10-01 16:38:35 +0530

    fdrydfhd

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -25,7 +25,7 @@ exports.getSingleProduct = async(req, res, next) => {
  if(!product) {
   return res.status(404).json({
     success: false,
-    message: "Product no found"
+    message: "Product not found"
   });
  }
 
@@ -33,4 +33,26 @@ exports.getSingleProduct = async(req, res, next) => {
     success:true,
     product
   })
+}
+
+//Update Product
+exports.updateProduct = async (req, res, next) =>{
+  let product = await Product.findById(req,params.id);
+
+  if(!product) {
+    return res.status(404).json({
+      success: false,
+      message: "Product not found"
+    });
+   }
+  
+   product = await Product.findByIdAndUpdate(req.params.id, req.body, {
+    new: true,
+    runValidators: true
+   })
+
+   res.status(200).json({
+    success: true,
+    product 
+   })
 }
--- backend/routes/product.js
+++ backend/routes/product.js
@@ -1,9 +1,14 @@
 const express = require('express');
-const { getProducts, newProduct, getSingleProduct } = require('../controllers/productController');
+const { getProducts, newProduct, getSingleProduct, updateProduct } = require('../controllers/productController');
 const router = express.Router();
 
 router.route('/products').get(getProducts);
 router.route('/product/new').post(newProduct);
-router.route('/product/:id').get(getSingleProduct);
+// router.route('/product/:id').get(getSingleProduct);
+// router.route('/product/:id').put(updateProduct);
+
+router.route('/product/:id')
+                            .get(getSingleProduct)
+                            .put(updateProduct)
 
 module.exports = router;
